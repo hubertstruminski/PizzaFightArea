a3c499a6796a1e3922a3fff11f45455fcb46b5b6
Hubert <hubert.struminski@gmail.com>
2020-06-11 21:18:15 +0200

    add Shape enum class

---
+++ PizzaFightArea/Enum/Shape.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PizzaFightArea.Enum
+{
+    enum Shape
+    {
+        CIRCLE, SQUARE, TRIANGLE, RECTANGLE
+    }
+}
--- PizzaFightArea/Model/Pizza.cs
+++ PizzaFightArea/Model/Pizza.cs
@@ -6,5 +6,11 @@ namespace PizzaFightArea.Model
 {
     class Pizza
     {
+        private string name { get; set; }
+        private int hunger { get; set; }
+        private int sharpness { get; set; }
+        private int flavor { get; set; }
+        private int shape { get; set; }
+
     }
 }
