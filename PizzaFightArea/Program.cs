4d0252e42ca6d0a54c608f6ebe709fe2eb2a297e
Hubert <hubert.struminski@gmail.com>
2020-06-12 14:06:58 +0200

    add MainView class with method for render main menu, create MenuResolver class template, add invoking static method to Program class

--- PizzaFightArea/Program.cs
+++ PizzaFightArea/Program.cs
@@ -1,5 +1,6 @@
 ﻿using PizzaFightArea.Model;
 using PizzaFightArea.Service;
+using PizzaFightArea.View;
 using System;
 using System.Collections.Generic;
 
@@ -7,16 +8,30 @@ namespace PizzaFightArea
 {
     class Program
     {
+        static void manageMenu()
+        {
+            MainView mainView = new MainView();
+            mainView.showMainMenu();
+        }
         static void Main(string[] args)
         {
+            manageMenu();
             PizzaInitializator initializator = new PizzaInitializator();
             List<Pizza> pizzas = initializator.pizzas;
 
-            Random random = new Random();
-            int randomIndex = random.Next(0, pizzas.Count);
-            Pizza pizza = pizzas[randomIndex];
-            Console.WriteLine(pizza.ToString("Hubert"));
-            Console.WriteLine(pizzas[15].ToString("Computer"));
+            //Random random = new Random();
+            //int randomIndex = random.Next(0, pizzas.Count);
+            //Pizza pizza = pizzas[randomIndex];
+            //Console.WriteLine(pizza.ToString("Hubert"));
+            //Console.WriteLine(pizzas[15].ToString("Computer"));
+
+            int count = 1;
+            foreach(Pizza pizza in pizzas)
+            {
+                Console.WriteLine(pizza.ToString("Hubert " + count));
+                count++;
+            }
+
         }
     }
 }
---
+++ PizzaFightArea/View/MainView.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PizzaFightArea.View
+{
+    public class MainView
+    {
+        public void showMainMenu()
+        {
+            Console.WriteLine("Pizza Fight Area");
+            Console.WriteLine("----------------");
+            Console.WriteLine("1. New game - 1 vs Computer");
+            Console.WriteLine("2. New game - 1 vs 1");
+            Console.WriteLine("3. Statistics");
+            Console.WriteLine("4. Exit");
+        }
+    }
+}
